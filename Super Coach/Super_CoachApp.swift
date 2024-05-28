c04ffa7fe7194be53026960bf9a9a9b5a5ba2bc1
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-05-28 14:16:04 +0200

    Initial Commit

---
+++ Super Coach/Super_CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_CoachApp.swift
+//  Super Coach
+//
+//  Created by Guillaume Demergès on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Super_CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
